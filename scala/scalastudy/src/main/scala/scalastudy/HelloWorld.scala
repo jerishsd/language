b0f41dd0a23b34d1e723f6a49143db5421d25ae0
jerry <jerry@geekbird.com>
2013-05-31 18:04:37 -0700

    Hello World addition

---
+++ scala/scalastudy/src/main/scala/scalastudy/HelloWorld.scala
@@ -0,0 +1,12 @@
+package scalastudy
+
+/**
+ * Hello world!
+ *
+ */
+object HelloWorld 
+{
+    def main(args: Array[String]):Unit={
+        println( "Hello World New!" )
+    }
+}
---
+++ scala/scalastudy/src/test/scala/scalastudy/HelloWorldTest.scala
@@ -0,0 +1,18 @@
+package scalastudy
+
+import org.junit.Test
+import org.junit.Assert._
+
+/**
+ * Unit test for simple App.
+ */
+class HelloWorldTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    def testHelloWorld():Unit={
+        assertTrue( true )
+    }
+}
+
